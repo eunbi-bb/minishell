fdbe6dececfe954c8a8a40971d40cff8b4f809ae
Eunbi Cho <eucho@f1r6s6.codam.nl>
2023-06-10 20:29:06 +0200

    init_parser added

--- src/parser.c
+++ src/parser.c
@@ -1,19 +1,30 @@
 #include "../includes/parser.h"
 
+void	init_parser_utils(t_lexer_utils *lexer, t_parser_utils *parser)
+{
+	t_redir_utils	redir;
+
+	redir.lexer_lst = lexer->token_list;
+	redir.redir = NULL;
+	redir.num_redir = 0;
+	redir.utils = parser;
+	return (redir);
+}
+
+
 int	parser(t_parser_utils *parser)
 {
-	t_tree_node			*node;
-	t_redir_utils	*redir;
+	t_parser_node	*node;
 	t_lexer_utils	*lexer;
 	
 	if (lexer->token_list->token == PIPE)
-		return (token_error);
+		return (token_error); 
 	while (lexer->token_list != NULL)
 	{
-		if (lexer->token_list->token = PIPE)
+		if (lexer->token_list->token == PIPE)
 			//Split tokens depends on pipe.
-			split_tokens();
-		
+			split_tokens(&lexer);
+		init_parser_utils(&lexer, &parser);
 	}
 	
 }
