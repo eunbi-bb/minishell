7913dacb7f5185263c0d6c4678931fc34eb0c830
Simona <simona.semancova@gmail.com>
2023-10-08 10:42:33 +0200

    export

--- src/builtins/cmd_export.c
+++ src/builtins/cmd_export.c
@@ -12,6 +12,71 @@
 
 #include "minishell.h"
 
+// Function to split the linked list into two halves
+void splitList(t_env *source, t_env **leftRef, t_env **rightRef) {
+    t_env *fast;
+    t_env *slow;
+
+    if (source == NULL || source->next == NULL) {
+        *leftRef = source;
+        *rightRef = NULL;
+    } else {
+        slow = source;
+        fast = source->next;
+
+        while (fast != NULL) {
+            fast = fast->next;
+            if (fast != NULL) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+
+        *leftRef = source;
+        *rightRef = slow->next;
+        slow->next = NULL;
+    }
+}
+
+
+t_env* merge(t_env *left, t_env *right) {
+    t_env *result = NULL;
+
+    if (left == NULL)
+        return right;
+    if (right == NULL)
+        return left;
+
+    if (strcmp(left->key, right->key) <= 0) {
+        result = left;
+        result->next = merge(left->next, right);
+    } else {
+        result = right;
+        result->next = merge(left, right->next);
+    }
+
+    return result;
+}
+
+// Merge Sort function for sorting the linked list
+t_env* mergeSort(t_env *head) {
+    if (head == NULL || head->next == NULL)
+        return head;
+
+    t_env *left;
+    t_env *right;
+    t_env *sorted;
+
+    splitList(head, &left, &right);
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    sorted = merge(left, right);
+
+    return sorted;
+}
+
 int var_exist(char *key, t_env *env)
 {
     size_t len;
@@ -49,13 +114,27 @@ void unset_var(t_env** head,char* key) {
     }
 }
 
+void printList(t_env *head) {
+    t_env *current = head;
+    while (current != NULL) {
+        printf("declare -x %s %s\n", current->key, current->value);
+        current = current->next;
+    }
+}
+
 void cmd_export(t_env **head, char *str)
 {
     t_env* newNode;
     char* equalSign;
     size_t keyLength;
     t_env* current;
+	t_env* sorted;
 
+	if (!str)
+	{
+		sorted = mergeSort(*head);
+		printList(sorted);
+	}
     newNode = (t_env*)malloc(sizeof(t_env));
     equalSign = ft_strchr(str, '=');
     keyLength = equalSign - str + 1;
