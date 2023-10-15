2785e5e0bfa56414bfbd5924b8634c618803d4ad
Simona Semancova <ssemanco@f0r3s24.codam.nl>
2023-10-15 14:27:09 +0200

    strcmp libft

--- libft/libft.h
+++ libft/libft.h
@@ -42,6 +42,7 @@ int		ft_atoi(const char *str);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strcmp(const char *str1, const char *str2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
