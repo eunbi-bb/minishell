c82eff21ddb0278d7f4d58e606a3eee9f09e0c24
eunbi-bb <eunbicho1995@gmail.com>
2023-10-19 00:15:03 +0200

    parser error fixed.

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: eucho <eucho@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/02 16:12:41 by eucho         #+#    #+#                 */
-/*   Updated: 2023/10/18 19:08:38 by eucho         ########   odam.nl         */
+/*   Updated: 2023/10/19 00:11:08 by eunbi         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,53 +32,56 @@ static int	count_args(t_tokens	*lexer)
 	return (arg_num);
 }
 
-static void	generate_redir(t_tokens *curr, t_cmd *cmd)
+void	generate_redir(t_tokens *current, t_cmd *cmd)
 {
 	t_tokens	*tmp;
 	t_redir		*new;
 
-	tmp = curr;
-	while (tmp && (tmp->token >= LESSER && tmp->token <= APPEND))
+	tmp = current;
+	while (tmp && tmp->token != PIPE)
 	{
 		new = create_redir_node();
 		add_after_redir(&cmd->redir, new);
 		new->redir_type = tmp->token;
 		if (tmp->token >= LESSER && tmp->token <= APPEND)
 		{
-			curr = curr->next;
+			current = current->next;
 			new->file_name = ft_strdup(tmp->next->data);
 			tmp = tmp->next;
-			curr = curr->next;
+			current = current->next;
 		}
 		tmp = tmp->next;
 	}
 }
 
-static t_cmd	*generate_cmd(t_tokens *tokens, t_cmd *cmd, int arg_num)
+t_cmd	*generate_cmd(t_tokens *tokens, t_cmd *cmd)
 {
+	int			arg_num;
 	int			i;
 	int			j;
 	size_t		len;
-	t_tokens	*curr;
+	t_tokens	*current;
 
 	i = 0;
 	j = 0;
-	curr = tokens;
+	current = tokens;
+	arg_num = count_args(current);
 	if (arg_num > 0)
 		cmd->data = ft_calloc((arg_num + 1), sizeof(char *));
-	generate_redir(curr, cmd);
-	while (i++ <= arg_num && curr)
+	generate_redir(current, cmd);
+	while (i <= arg_num && current)
 	{
-		if (curr->data && (curr->token == DEFAULT || curr->token >= DOLLAR))
+		if (current->data != NULL && (current->token == DEFAULT || current->token >= DOLLAR))
 		{
-			len = ft_strlen(curr->data) + 1;
+			len = ft_strlen(current->data) + 1;
 			cmd->data[j] = ft_calloc(len, sizeof(char));
-			ft_strlcpy(cmd->data[j], curr->data, len);
+			ft_strlcpy(cmd->data[j], current->data, len);
 			j++;
 		}
-		if ((curr->token >= LESSER && curr->token <= APPEND) || curr->token == PIPE)
-			curr = curr->next;
-		curr = curr->next;
+		i++;
+		if (current->token >= LESSER && current->token <= APPEND)
+			current = current->next;
+		current = current->next;
 	}
 	return (cmd);
 }
@@ -88,24 +91,23 @@ static t_cmd	*generate_cmd(t_tokens *tokens, t_cmd *cmd, int arg_num)
 */
 void	parser(t_lexer *lexer, t_parser *parser)
 {
-	t_tokens	*curr;
+	t_tokens	*current;
 	t_cmd		*cmd;
-	int			arg_num;
 
-	curr = lexer->token_list;
-	arg_num = count_args(curr);
-	while (curr)
+	current = lexer->token_list;
+	while (current)
 	{
-		if (curr->token != PIPE)
+		if (current->token != PIPE)
 		{
 			cmd = create_cmd_node();
-			generate_cmd(curr, cmd, arg_num);
+			generate_cmd(current, cmd);
 			add_after_cmd(&parser->cmd_list, cmd);
-			while (curr->token != PIPE && curr->next)
+			while (current->token != PIPE && current->next)
 			{
-				curr = curr->next;
+				current = current->next;
 			}
 		}
-		curr = curr->next;
+		current = current->next;
 	}
 }
+
