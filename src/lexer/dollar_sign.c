2b33d8d0f0a8c94e01fd155b00cbc7680d3b0779
eunbi-bb <eunbicho1995@gmail.com>
2023-10-28 17:29:51 +0200

    memory leak on replace_between

--- src/lexer/dollar_sign.c
+++ src/lexer/dollar_sign.c
@@ -6,7 +6,7 @@
 /*   By: eunbi <eunbi@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/09 19:06:41 by eunbi         #+#    #+#                 */
-/*   Updated: 2023/10/27 22:57:40 by eunbi         ########   odam.nl         */
+/*   Updated: 2023/10/28 17:26:36 by eunbi         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,98 +67,107 @@ char	*remove_quotes(char *str)
 	return (result);
 }
 
+// Replace the value between the indexes with its ENV value.
+char *replace_between(t_parser *parser, char *data, int begin, int end)
+{
+	char 	*search;
+	char	*begin_str;
+	char	*end_str;
+	char	*tmp;
+	char	*result;
+
+	if (begin < 0)
+		return data;
+	if (end < 0)
+		end = ft_strlen(data);
+	tmp = ft_substr(data, begin, end - begin + 1);
+	search = expand(tmp, parser->env);
+	free(tmp);
+	begin_str = ft_substr(data, 0, begin);
+	end_str = ft_substr(data, end + 1, ft_strlen(data));
+	tmp = ft_strjoin(begin_str, search);
+	free(begin_str);
+	free(search);
+	result = ft_strjoin(tmp, end_str);
+	free(tmp);
+	free(end_str);
+	return (result);
+}
+
+// Find the dollar sign value, and replace it with the ENV value.
 char *replacer(char *str, t_parser *parser)
 {
 	char		quote;
-	char		*expand_value;
 	int			i;
-	int			begin_expand = -1;
-	int			end_expand = -1;
 	int 		next;
 
 	i = 0;
-	quote = '\0';
-	while (str[i] && end_expand < 0)
+	while (str[i])
 	{
 		if (str[i] == '\'' || str[i] == '\"')
 		{
 			quote = str[i];
 			next = i + next_quote(str, i);
-			while (i < next && end_expand < 0)
+			while (i < next)
 			{
 				if (str[i] == '$' && quote == '\"')
-				{
-					begin_expand = i;
-					end_expand = get_len_dollar(str, begin_expand);
-				}
-				else
-					i++;
+					return replace_between(parser, str, i, get_len_dollar(str, i));
+				i++;
 			}
 		}
 		else if (str[i] == '$')
-		{
-			begin_expand = i;
-			end_expand = get_len_dollar(str, begin_expand);
-			i += end_expand;
-		}
+			return replace_between(parser, str, i, get_len_dollar(str, i));
 		i++;
 	}
-	if (begin_expand < 0)
-		return str;
-	if (end_expand < 0)
-		end_expand = ft_strlen(str);
-	char *found_value = NULL;
-	found_value = ft_substr(str, begin_expand, end_expand - begin_expand + 1);
-	char *search = expand(found_value, parser->env);
-	expand_value = ft_strdup(search);
-	free(search);
-	free(found_value);
-	char	*begin = ft_substr(str, 0, begin_expand);
-	char	*end = ft_substr(str, end_expand + 1, ft_strlen(str));
-	char	*tmp = ft_strjoin(begin, expand_value);
-	char	*result = ft_strjoin(tmp, end);
-	free(expand_value);
-	free(tmp);
-	free(begin);
-	free(end);
-	return (result);
+	return str;
 }
 
-void	determine_expanding(t_lexer *lexer, t_parser *parser)
+// Loop over the data until all values with a dollar sign are replaced.
+// Before returning, remove all quotes from the string.
+char *resolver(char *data, t_parser *parser)
 {
-	t_tokens	*head;
 	char		*result;
 	char 		*str;
 	char 		*tmp;
 
-	head = lexer->token_list;
-	result = ft_strdup("");
+	str = data;
+	tmp = str;
+	result = "";
+	while (ft_strcmp(str, result) != 0)
+	{
+		// free(str);
+		str = tmp;
+		// free(result);
+		result = replacer(str, parser);
+		tmp = result;
+	}
+	// str = NULL;
+	// if (str != NULL)
+	// 	free(str);
+	// free(tmp);
+	return (remove_quotes(result));
+}
+
+// Loop over all arguments to check for dollar signs to replace.
+void	determine_expanding(t_lexer *lexer, t_parser *parser)
+{
+	t_tokens	*head;
+	char 		*tmp;
+
 	tmp = NULL;
-	str = NULL;
+	head = lexer->token_list;
 	while (lexer->token_list)
 	{
 		if (lexer->token_list->token == DEFAULT)
 		{
+			tmp = resolver(lexer->token_list->data, parser);
+			free(lexer->token_list->data); // TODO maybe can be removed
 			if (tmp != NULL)
-				free(tmp);
-			str = ft_strdup(lexer->token_list->data);
-			tmp = ft_strdup(str);
-			while (ft_strcmp(str, result) != 0)
-			{
-				free(str);
-				str = ft_strdup(tmp);
-				free(result);
-				result = replacer(str, parser);
-				free(tmp);
-				tmp = ft_strdup(result);
-			}
-			free(lexer->token_list->data);
-			lexer->token_list->data = remove_quotes(result);
+				lexer->token_list->data = tmp;
+			else
+				lexer->token_list->data = NULL;
 		}
 		lexer->token_list = lexer->token_list->next;
 	}
-	if (str)
-		free(str);
-	free(tmp);
 	lexer->token_list = head;
 }
